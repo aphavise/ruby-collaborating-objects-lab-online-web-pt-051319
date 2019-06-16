b7746162d6712381c0de912764741ecb70844b57
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-16 06:52:35 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class MP3Importer
+  
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path =path
+  end
+  
+  def files 
+    # return all fire names in given path as an array
+    Dir.entries(path).filter { |filename| filename.end_with?('.mp3')}
+  end
+
+  
+  def import 
+    files.each{|file| Song.new_by_filename(file)}
+  end
+  
+end 
