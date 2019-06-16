de23cc9945b7ab9aa5f0e1e8f823b0be1efa466a
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-16 06:17:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,7 +29,10 @@ class Artist
     #self.new(name).tap{|artist| artist.save}
     # artist = self.new(name)
     # artist.save
+    #   tap{|artist| artist.save} = tap(&:save)
     self.new(name).tap(&:save)
+    
+    
   end
   
   def self.find_by_name(name)
