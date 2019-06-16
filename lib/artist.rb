402fab136853c67510dcfe38813ac38203a95f85
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-16 06:06:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
     songs << song
   end
   
-  def self.find_or_create_by_name  # composite constructor 
+  def self.find_or_create_by_name(name)  # composite constructor 
     self.find_by_name(name) || self.create_by_name(name)
   end
   
